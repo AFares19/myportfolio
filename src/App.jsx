5f987dd3d12a732943abacee82595c6fe10ba693
AFares19 <81560276+AFares19@users.noreply.github.com>
2023-06-30 08:25:33 +0000

    changes made matching my profile

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fares Arouay",
+  title: "Java Software Engineer",
+  email: "arouay.f@gmail.com",
+  gitHub: "",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/fares-a-9337a21b9",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,23 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software engineer passionate about Java development and any related technologies especially the new modern Java dvelopment: Cloud native apps. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Problem Solving",
+  "Object Oriented Programming",
+  "Functional Programming",
+  "Java",
+  "Spring framework",
+  "Sql",
+  "Maven",
+  "Git",
+  "Jenkins",
+  "Apache Tomcat",
 ];
 
 /**
@@ -45,7 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I enjoy writing code respecting the proper object oriented programming principles and always eager to learn new ways and practices that help me produce flexible and maintainable code.";
 
 const About = () => {
   return (
